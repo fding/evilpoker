0ca1c09a5c3090ffe6ff4a337150c8751e297b62
David Ding <davidding2000@gmail.com>
2016-04-05 16:53:42 -0400

    download script, finish training data creation script

--- hand_eval/create_training_set.py
+++ hand_eval/create_training_set.py
@@ -5,11 +5,21 @@ from multiprocessing import Pool
 
 import argparse
 import glob
+import itertools
 
 hdb = {}
 roster = {}
 players = {}
 value_dict = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+
+def encode(a):
+    if a == 'r' or a == 'b':
+        return [0, 0, 1]
+    if a == 'c' or a == 'k':
+        return [0, 1, 0]
+    if a == 'f':
+        return [1, 0, 0]
+    raise Exception('Saw %s' % a)
 
 def card_value(c):
     return value_dict[c[0]]
@@ -50,8 +60,8 @@ def count(l):
     return counts.values()
 
 
-def calculate_features(nplayers, hole_cards, table_cards):
-    potential = eval_hand_potential(nplayers), hole_cards, table_cards)
+def calculate_features(nplayers, hole_cards, table_cards, bets, action):
+    potential = eval_hand_potential(nplayers, hole_cards, table_cards)
     hole_values = map(card_value, hole_cards)
     max_hole_value = max(hole_values)
     other_hole_value = min(hole_values)
@@ -69,18 +79,142 @@ def calculate_features(nplayers, hole_cards, table_cards):
         highest_triple = val
 
     flush_potential = max(count(map(suit, hole_cards + table_cards)))
-    table_flush_potential = max(count(map(suit, table_cards)))
+    table_flush_potential = max(count(map(suit, table_cards) + [0]))
 
-    return [potential, max_hole_value, other_hole_value, tvsum, highest_pair, highest_triple,
-            flush_potential, table_flush_potential]
+    return [nplayers, potential, max_hole_value, other_hole_value, tvsum, highest_pair, highest_triple,
+            flush_potential, table_flush_potential] + list(bets[:-1]) + list(bets[-1]) + encode(action)
 
+def calculate_bet_structure(nplayers, pot_at_round_begin, chips_at_round_begin,
+                            my_chips_in_pot_at_round_begin, diff, sequence):
+    units_per_bet = 0
+    nunits = 0
+    bet_seq = {}
+    last_bet_size = {}
+    chips = {}
+
+    bet_structure = {}
+    remain = {}
+    for p in range(nplayers):
+        last_bet_size[p] = 0
+        bet_seq[p] = []
+        bet_structure[p] = []
+        chips[p] = 0
+        remain[p] = True
+
+    nremain = nplayers
+    for position, (p, a) in enumerate(sequence):
+        betsize = units_per_bet - last_bet_size[p]
+        tocall = betsize
+        if a in {'B', 'r', 'b'}:
+            units_per_bet += 1
+            betsize = units_per_bet - last_bet_size[p]
+            nunits += betsize
+            chips[p] -= betsize
+        elif a in {'k', 'c'}:
+            nunits += betsize
+            chips[p] -= betsize
+        elif a in {'f', 'Q'}:
+            betsize = 0
+            nremain -= 1
+            remain[p] = False
+        else:
+            betsize = 0
+
+        bet_seq[p].append((nremain, nunits, betsize, tocall, position,
+                           [chips[i] for i in range(nplayers)],
+                           [(chips[i] if remain[p] else 1) for i in range(nplayers)]
+                          ))
+        last_bet_size[p] = units_per_bet
+
+    if nunits > 0:
+        unitsize = float(diff)/nunits
+    else:
+        unitsize = 0
+
+    my_chips_in_pot = {}
+    check = {}
+    for p in range(nplayers):
+        last_bet_size[p] = 0
+        my_chips_in_pot[p] = my_chips_in_pot_at_round_begin[p]
+        bet_structure[p] = []
+        check[p] = (chips_at_round_begin[p] - chips[p]) * unitsize
+        for nremain, nunits, betsize, tocall, position, chips_all, chips_remain in bet_seq[p]:
+            my_chips_in_pot[p] += unitsize * betsize
+            bet_structure[p].append((my_chips_in_pot[p], 
+                                     nunits * unitsize,
+                                     tocall * unitsize,
+                                     position,
+                                     nremain,
+                                     # [c * unitsize for c in chips_all],
+                                     [(chips_at_round_begin[p] + c * unitsize if c != 1 else 0) for c in chips_remain]))
+
+    return bet_structure
 
 def process_hand(hand_id):
-    player_list = roster[hand_id][1]
-    for player_name in player_list:
-        p = players[player_name][hand_id]
-        if p[-1]:
-            print calculate_features(hdb[hand_id], p)
+    try:
+        last_pot = 0
+        last_nplayers = int(hdb[hand_id][2])
+        chips_in_pot = {}
+        chips = {}
+        player_list = roster[hand_id][1]
+        for p in player_list:
+            chips_in_pot[p] = 0
+            chips[p] = int(players[p][hand_id][6])
+
+        for roundi, round in enumerate(hdb[hand_id][3:7]):
+            if round == '0/0':
+                return
+            nplayers = int(round.split('/')[0])
+            pot = int(round.split('/')[1])
+            diff = pot - last_pot
+            last_pot, pot = pot, last_pot
+            last_nplayers, nplayers = nplayers, last_nplayers
+            chips_in_pot_round = {}
+            chips_round = {}
+            counter = 0
+            name_to_id = {}
+            id_to_name = {}
+            actions = []
+            for player_name in player_list:
+                p = players[player_name][hand_id]
+                if p[2 + roundi] != '-':
+                    name_to_id[player_name] = counter
+                    id_to_name[counter] = player_name
+                    chips_in_pot_round[counter] = chips_in_pot[player_name]
+                    chips_round[counter] = chips[player_name]
+                    actions.append((int(p[1]), counter, p[2 + roundi]))
+                    counter += 1
+            actions.sort()
+
+            sequence = []
+            for t in itertools.izip_longest(*[a[2] for a in actions], fillvalue='-'):
+                for i, a in enumerate(t):
+                    sequence.append((actions[i][1], a))
+
+            bet_structure= calculate_bet_structure(nplayers, pot, chips_round, chips_in_pot_round, diff, sequence)
+
+            for p in range(nplayers):
+                chips_in_pot[id_to_name[p]] = bet_structure[p][-1][0]
+                chips[id_to_name[p]] = bet_structure[p][-1][-1][p]
+
+
+            for player_name in player_list:
+                p = players[player_name][hand_id]
+                if player_name in name_to_id:
+                    for bet, a in itertools.izip_longest(bet_structure[name_to_id[player_name]], p[2+roundi], fillvalue='-'):
+                        if a not in {'b', 'c', 'k', 'r', 'f'}:
+                            continue
+                        if roundi == 0:
+                            print ' '.join(map(str, calculate_features(nplayers, p[-1], [], bet, a)))
+                        elif roundi == 1:
+                            print ' '.join(map(str, calculate_features(nplayers, p[-1], hdb[hand_id][-1][:3], bet, a)))
+                        elif roundi == 2:
+                            print ' '.join(map(str, calculate_features(nplayers, p[-1], hdb[hand_id][-1][:4], bet, a)))
+                        elif roundi == 3:
+                            print ' '.join(map(str, calculate_features(nplayers, p[-1], hdb[hand_id][-1][:5], bet, a)))
+    except Exception as e:
+        print >>sys.stderr, e
+
 
 value_to_name = {
     0: '2',
