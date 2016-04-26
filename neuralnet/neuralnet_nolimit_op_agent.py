476c019723dd848998796a62c063f1dc8575cca8
Serena Wang <serenalwang@gmail.com>
2016-04-26 17:27:00 -0400

    opponent modeling is WORKING and BEATING SAFE WHAAAAT

--- neuralnet/neuralnet_nolimit_op_agent.py
+++ neuralnet/neuralnet_nolimit_op_agent.py
@@ -8,7 +8,254 @@ from pokernet_nolimit import PokerNetNoLimit
 
 import argparse
 import numpy as np
+import math
+import time
 
+class State(object):
+    def __init__(self):
+        self.hands = [[], []]
+        self.boardcards = []
+        self.hiddenboardcards = []
+        self.actions = []
+        self.pot = 0
+        self.chips_per_player = [0, 0]
+        self.betsize = 0
+        self.neural_nets = {}
+        
+    def savestate(self):
+        self.oldboardcards = self.boardcards[:]
+        self.oldpot = self.pot
+        self.oldchips_per_player = self.chips_per_player[:]
+        self.oldbetsize = self.betsize
+
+    def restorestate(self):
+        self.boardcards = self.oldboardcards[:]
+        self.pot = self.oldpot
+        self.betsize = self.oldbetsize
+        self.chips_per_player = self.oldchips_per_player[:]
+
+    def step(self, curplayer, ncallsinrow, nraiseinrow, pos):
+        pot_features = [self.chips_per_player[curplayer] * 0.01, self.pot * 0.01, (self.betsize - self.chips_per_player[curplayer]) * 0.01, 2, pos]
+        card_features = poker.calculate_card_features(2, self.hands[curplayer], self.boardcards)
+
+        cur_neural_net = self.neural_nets[curplayer]
+        
+        # Generate opponent moves
+        action_output = cur_neural_net.eval(2, card_features, pot_features, [.5,.5])[0]
+        action_probabilities = action_output[:3]
+        action_probabilities[0] *= 20
+
+        if nraiseinrow[curplayer] >= 1:
+            action_probabilities[2] = 0
+        
+        s = sum(action_probabilities)
+        action_probabilities = action_probabilities / s
+        at = np.random.choice([poker.FOLD, poker.CALL, poker.RAISE], 1, p=action_probabilities)[0]
+        rs = int(action_output[3])
+        
+        nremaining = 2
+        self.doaction(curplayer, at, rs)
+        if at == poker.RAISE:
+            ncallsinrow = 0
+            nraiseinrow[curplayer] += 1
+        elif at == poker.CALL:
+            ncallsinrow += 1
+            nraiseinrow[curplayer] = 0
+        else:
+            nremaining -= 1
+
+        return ncallsinrow, nraiseinrow, nremaining
+
+    def run_round_to_finish(self, curplayer, ncallsinrow, pos):
+        # First, finish current round
+        nremaining = 2
+        nraiseinrow = [0, 0]
+        while ncallsinrow < 2:
+            ncallsinrow, nraiseinrow, nremaining = self.step(curplayer, ncallsinrow, nraiseinrow, pos)
+            pos += 1
+            curplayer = 1 - curplayer
+            if nremaining == 1:
+                break
+        return curplayer, nremaining
+
+    def score(self, who, whowon):
+        if who == whowon:
+            return self.pot - self.chips_per_player[who]
+        else:
+            return -self.chips_per_player[who]
+
+    def doaction(self, curplayer, action_type, raise_size):
+        if action_type == poker.RAISE:
+            self.pot += raise_size - self.chips_per_player[curplayer]
+            self.chips_per_player[curplayer] = raise_size
+            self.betsize = raise_size
+        elif action_type == poker.CALL:
+            self.pot += self.betsize - self.chips_per_player[curplayer]
+            self.chips_per_player[curplayer] = self.betsize
+
+def dprint(fmt, *args):
+    # To disable debug printing, change this to false
+    if True:
+        print (('[Opponent Modeling] [ts %s]' % time.time()) + fmt) % args
+        sys.stdout.flush()
+
+def simulate(my_id, game, state, nets, action_probabilities, raise_size, chips_to_call, minraise):
+    t_start = time.time()
+    dprint('Started at %s', t_start)
+    deck = poker.Deck()
+    for card in poker.get_board_cards(game, state):
+        deck.seen(poker.card_to_int(card))
+    for card in poker.get_hole_cards(game, state, my_id):
+        deck.seen(poker.card_to_int(card))
+
+    raise_sizes = [raise_size * 0.5, raise_size * 0.75, raise_size, raise_size * 1.25, raise_size * 1.5, raise_size * 2, raise_size * 3, raise_size * 5, raise_size * 10, raise_size * 100]
+    raise_sizes = [int(r) for r in raise_sizes if r > minraise]
+    raise_num = [1.0 for _ in raise_sizes]
+    raise_num[-1] = 0.05
+    raise_num[-2] = 0.2
+    raise_denom = sum(raise_num)
+
+    action_num = [int(a * 16) + 1 for a in action_probabilities]
+    # Folding is invalid in this case
+    if chips_to_call == 0:
+        action_num[0] = 0
+    action_denom = sum(action_num)
+
+    for i in range(100):
+        dprint('Round %d. My id=%d', i, my_id)
+        # Assign random cards
+        s = State()
+        logpval = 0
+        deck.reset()
+        s.neural_nets = nets
+        s.boardcards = poker.get_board_cards(game, state)
+        s.hands[my_id] = poker.get_hole_cards(game, state, my_id)
+        s.hands[1 - my_id].append(poker.int_to_card(deck.draw().serialize()))
+        s.hands[1 - my_id].append(poker.int_to_card(deck.draw().serialize()))
+
+        s.hiddenboardcards = s.boardcards + [poker.int_to_card(deck.draw().serialize()) for _ in range(5 - len(s.boardcards))]
+
+        dprint('Round %d. Hole cards: [%s] [%s]. Board cards: [%s]. Hidden cards: [%s]',
+               i, ';'.join(s.hands[0]), ';'.join(s.hands[1]), ';'.join(s.boardcards), ';'.join(s.hiddenboardcards))
+
+        # Replay game up to current state
+        # First, add the blinds
+        s.pot += poker.getBlind(game, 0)
+        s.pot += poker.getBlind(game, 1)
+        s.chips_per_player[0] = poker.getBlind(game, 0)
+        s.chips_per_player[1] = poker.getBlind(game, 1)
+
+        s.betsize = max(poker.getBlind(game, 0), poker.getBlind(game, 1))
+
+        dprint('Round %d. After blinds, pot=%d, chips[0]=%d, chips[1]=%d, betsize=%d',
+               i, s.pot, s.chips_per_player[0], s.chips_per_player[1], s.betsize)
+
+        pos = 0
+        for r in range(state.round + 1):
+            s.actions.append([])
+            pos = 0
+            for a in range(poker.getNumActions(state, r)):
+                act = poker.getAction(state, r, a)
+                s.actions[-1].append(act)
+                curplayer = poker.getActingPlayer(state, r, a)
+                pot_features = [s.chips_per_player[curplayer], s.pot, (s.betsize - s.chips_per_player[curplayer]), 2, pos]
+                card_features = poker.calculate_card_features(2, s.hands[curplayer], s.boardcards)
+                dprint('Round %d. Action=(%d,%d). curplayer=%d', i, act.type, act.size, curplayer)
+                pos += 1
+                s.doaction(curplayer, act.type, act.size)
+                logpval += math.log(nets[curplayer].eval(2, card_features, pot_features, [])[0][act.type]) - math.log(0.34)
+
+        s.savestate()
+
+        # Current and future actions
+        for j in range(10):
+            s.restorestate()
+
+            # Choose an action at random
+            probabilities = [a / float(action_denom) for a in action_num]
+            action_type = np.random.choice([poker.FOLD, poker.CALL, poker.RAISE], 1, p=probabilities)[0]
+            raise_dist = [r / float(raise_denom) for r in raise_num]
+            dprint('raise_dist = [%s]', ';'.join(map(str, raise_dist)))
+            raise_size = np.random.choice(raise_sizes, 1, p=raise_dist)[0]
+
+            dprint('Round %d, hand %d. Action=(%d, %d), probabilities=[%s]', i, j, action_type, raise_size, ';'.join(map(str, probabilities)))
+            # TODO: check move validity
+
+            # evaluate move
+            score = 0
+
+            # fold: automatic loss
+            if action_type == poker.FOLD:
+                score = s.score(my_id, 1 - my_id)
+                score = - s.chips_per_player[my_id]
+            else:
+                at = action_type
+                rs = raise_size
+                curplayer = my_id
+                pos += 1
+                s.doaction(curplayer, at, rs)
+                curplayer = 1 - curplayer
+
+                ncallsinrow = 0
+                nremaining = 2
+                if at == poker.CALL:
+                    ncallsinrow += 1
+                    if len(s.actions) > 0 and len(s.actions[-1]) > 0 and s.actions[-1][-1].type == poker.CALL:
+                        ncallsinrow += 1
+
+                # First, finish current round
+                curplayer, nremaining = s.run_round_to_finish(curplayer, ncallsinrow, pos)
+
+                if nremaining == 1:
+                    score = s.score(my_id, 1 - curplayer)
+                else:
+                    # Play out the other rounds
+                    for r in range(state.round + 1, game.numRounds):
+                        s.boardcards = s.hiddenboardcards[0: len(s.boardcards) + poker.getNumBoardCards(game, r)]
+                        pos = 0
+                        curplayer = poker.getFirstPlayer(game, r)
+                        ncallsinrow = 0
+
+                        curplayer, nremaining = s.run_round_to_finish(curplayer, ncallsinrow, pos)
+                        if nremaining == 1:
+                            score = s.score(my_id, 1 - curplayer)
+                    
+                    # Showdown
+                    if nremaining == 2:
+                        scores = [poker.score_hand(s.hands[0], s.boardcards), 
+                                  poker.score_hand(s.hands[1], s.boardcards)]
+                        dprint('Round %d, hand %d. Showdown! hands[0]=%s, hands1[0]=%s, board=%s, scores=[%d, %d]', i, j, s.hands[0], s.hands[1], s.boardcards, scores[0], scores[1])
+                        score = s.score(my_id, my_id if scores[my_id] >= scores[1 - my_id] else 1 - my_id)
+            
+            # Update probabilities
+
+            inc = abs(score) * math.exp(logpval) * 0.01
+            action_denom += inc
+            if score < 0:
+                dprint('Round %d, hand %d. inc = %s, logpval = %.4f, pot=%s, score=%s, chips=%s', i, j, inc, logpval, s.pot, score, s.chips_per_player[my_id])
+                if action_type == poker.RAISE:
+                    raise_denom += inc
+                    for ind, size in enumerate(raise_sizes):
+                        if size != raise_size:
+                            raise_num[ind] += inc / float(len(raise_sizes) - 1)
+                for ind in range(len(action_num)):
+                    if ind != action_type:
+                        action_num[ind] += inc / 2.0
+            else:
+                dprint('Round %d, hand %d. inc = %s, logpval = %.4f, pot=%s, score=%s, chips=%s', i, j, inc, logpval, s.pot, score, s.chips_per_player[my_id])
+                action_num[action_type] += inc
+                if action_type == poker.RAISE:
+                    raise_denom += inc
+                    raise_num[raise_sizes.index(raise_size)] += inc
+
+    probabilities = [a / float(action_denom) for a in action_num]
+    dprint('Round %d. Hole cards: [%s]. Board cards: [%s]', i, ';'.join(s.hands[my_id]), ';'.join(s.boardcards))
+    dprint('Finished. Initial probabilities=[%s]. Final denom=%d, Final probabilities=[%s]', ';'.join(map(str,action_probabilities)), action_denom, ';'.join(map(str, probabilities)))
+    dprint('Ended. Took %s seconds.', time.time() - t_start)
+
+    return probabilities, raise_sizes[raise_num.index(max(raise_num))]
+
+            
 class NeuralNetNolimitOpAgent(PokerBot):
     import cProfile
     def __init__(self, host, port, gamefile, paramf):
@@ -58,6 +305,13 @@ class NeuralNetNolimitOpAgent(PokerBot):
         s = sum(action_probabilities)
         action_probabilities = action_probabilities / s
         raise_amount = int(action_output[3])
+        raisevalid, minsize, maxsize = poker.raiseIsValid(self.game, state)
+        
+        simulated_agents = {}
+        simulated_agents[my_id] = self.neural_net
+        simulated_agents[1 - my_id] = self.neural_net_op
+        
+        action_probabilities, raise_amount = simulate(my_id, self.game, state, simulated_agents, action_probabilities, raise_amount, chips_to_call, minsize)
         
         #print card_features
         #print action_output[:3]
@@ -67,7 +321,6 @@ class NeuralNetNolimitOpAgent(PokerBot):
         action.type = np.random.choice(self.actions, 1, p=action_probabilities)[0]
         action.size = 0
 
-        raisevalid, minsize, maxsize = poker.raiseIsValid(self.game, state)
         if action.type == poker.RAISE and raisevalid:
             if raise_amount < minsize:
                 action.size = minsize
@@ -81,6 +334,7 @@ class NeuralNetNolimitOpAgent(PokerBot):
             action.type = poker.CALL
         if action.type == poker.RAISE and self.prev_action == poker.RAISE:
             action.type = poker.CALL
+            action.size = 0
         #print action.type, action.size
         self.prev_action = action.type	
         assert(poker.isValidAction( self.game, state, 0, action ) > 0)
---
+++ neuralnet/play_nolimit_op_agent.sh
@@ -0,0 +1,1 @@
+python neuralnet/neuralnet_nolimit_op_agent.py --dealer_host=$2 --dealer_port=$3 --game_file=game/holdem.nolimit.2p.game --param_file=agent_params_nolimit/$1.npz
